86837a7388c390854d864f8ffb16ecfc1c387db2
HenryQuillin <henryquillin@gmail.com>
2020-09-24 12:59:40 -0500

    Added UI

--- trip_planner.py
+++ trip_planner.py
@@ -4,6 +4,12 @@ Henry Quillin
 Python Trip Planner
 '''
 
-print('Welcome to your vacation planner')
+print('Welcome to Vacation Planner')
 cust_name = input('Whats your Name? ')
-print('Nice to meet you ', cust_name)
+destination = input('Nice to meet you ' + cust_name + ', where are you traveling to? ')
+print('Great! ' + destination + ' sounds like a great trip')
+print('°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø')
+
+num_of_days = int(input('How many days are you going to spend traveling? '))
+budget = input('How much money are you going to spend on this trip? ')
+
